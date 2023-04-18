e794bcf846ce83116938001b47d8bed0c6695464
dracu-lah <nevilnicks4321@gmail.com>
2023-04-18 15:00:46 -0700

    added auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -15,28 +15,20 @@ export default NextAuth({
       async authorize(credentials, req) {
         // Add logic here to look up the user from the credentials supplied
 
-        // const res = await fetch("http://localhost:8000/auth/ login", {
-        //   method: "POST",
-        //   headers: {
-        //     "Content-Type": "application/json",
-        //   },
-        //   body: JSON.stringify({
-        //     username: credentials?.username,
-        //     password: credentials?.password,
-        //   }),
-        // });
-        // const user = await res.json();
-        // if (
-        //   credentials.username == "admin" ||
-        //   credentials.password == "admin"
-        // ) {
-        // }
-        const userCheck = credentials?.username;
-        const passCheck = credentials?.password;
-        let user = null;
-        if (passCheck == "admin" && userCheck == "admin") {
-          user = { name: "admin" };
-        }
+        const res = await fetch(
+          "https://result-management-node-production.up.railway.app/auth/users",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              username: credentials?.username,
+              password: credentials?.password,
+            }),
+          }
+        );
+        const user = await res.json();
 
         if (user) {
           return user;
@@ -46,4 +38,17 @@ export default NextAuth({
       },
     }),
   ],
+  session: {
+    strategy: "jwt",
+  },
+  jwt: {
+    async encode(params) {
+      // return a custom encoded JWT string
+      return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+    },
+    async decode(params) {
+      // return a `JWT` object, or `null` if decoding failed
+      return {};
+    },
+  },
 });
